69c88242e8dbe971ba9971b98ba1aaec0d7fe167
Rhenald <renaldkarel@gmail.com>
2022-09-19 21:00:04 +0700

    feat: creating links mutation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,9 @@
+// eslint-disable-next-line no-unused-vars
 import LinkList from "components/LinkList";
+import CreateLink from "components/CreateLink";
 
 function App() {
-  return <LinkList />;
+  return <CreateLink />;
 }
 
 export default App;
